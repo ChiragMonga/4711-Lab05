03f9972c4b90bbebd18b576e27862365dd6d5f0a
ChiragMonga <chiragmonga99@gmail.com>
2019-10-28 14:17:40 -0700

    another commit

--- app.js
+++ app.js
@@ -1,43 +1,23 @@
 let express = require('express');
 let app = express();
 let fs = require('fs');
-let bodyParser = require('body-parser');
-var cookieParser = require('cookie-parser');
-var exphbs  = require('express-handlebars');
 
+var exphbs  = require('express-handlebars');
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
+
+let bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
 app.use(cookieParser());
 
-app.get('/', (req,res) => {
-
-    fs.readFile('users.json', (err,data) => {
-        if(err) throw err;
-        let content = JSON.parse(data);
-        res.render("index", {user: content});
-    });
-});
-
-app.post('/delete', (req,res) => {
-    let content = fs.readFileSync('users.json');
-    content = JSON.parse(content);
-    for(let i = 0; i < content.length; i++) {
-        if(content[i].name === req.body.name) {
-            content.splice(i,1);
-        }
-    }
-    fs.writeFileSync('users.json', JSON.stringify(content, null, 2));
-    res.redirect('/');
-})
-
 app.post('/postAction', (req, res) => {
-    
+
     let userData = [];
-    
+
     user = 
     {
         "name": req.body.name,
@@ -54,14 +34,36 @@ app.post('/postAction', (req, res) => {
 
         fs.writeFile('./users.json', userData, err => {
             if (err) {
-                console.log('Error writing file', err)
-            } else {
-                console.log('Successfully wrote file')
+                console.log('Error writing file', err);
+            } 
+            else {
+                console.log('Successfully wrote file');
             }
-       })
+       });
     });
 
     res.redirect('/');
+});
+
+app.post('/delete', (req,res) => {
+    let content = fs.readFileSync('users.json');
+    content = JSON.parse(content);
+    for(let i = 0; i < content.length; i++) {
+        if(content[i].name === req.body.name) {
+            content.splice(i,1);
+        }
+    }
+    fs.writeFileSync('users.json', JSON.stringify(content, null, 2));
+    res.redirect('/');
+});
+
+app.get('/', (req,res) => {
+
+    fs.readFile('users.json', (err,data) => {
+        if(err) throw err;
+        let content = JSON.parse(data);
+        res.render("index", {user: content});
+    });
 });
 
 app.listen(3005, () => console.log('Server ready'));
